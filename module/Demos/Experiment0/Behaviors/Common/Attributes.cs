097a250c4a8df79df3206116e15455c64264690e
JayO <bluejayvrstudio@gmail.com>
2025-03-25 14:18:20 -0500

    working on experiment0

---
+++ module/Demos/Experiment0/Behaviors/Common/Attributes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InfiniteTree
+{
+    public abstract class Attributes
+    {
+        public float Health = 100;
+        public float Cash = 5;
+        public float MoveSpeed = 2.3f;
+    }
+}
---
+++ module/Demos/Experiment0/Behaviors/EMSBehaviors/EMSAttributes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InfiniteTree
+{
+    public class EMSAttributes : Attributes
+    {
+        
+    }
+}

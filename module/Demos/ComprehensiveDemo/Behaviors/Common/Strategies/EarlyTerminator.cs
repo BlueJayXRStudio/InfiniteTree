f6cbb8948c17730921f31798c8abd3f4635e0f49
JayO <bluejayvrstudio@gmail.com>
2025-03-29 04:26:32 -0500

    update

--- module/Demos/ComprehensiveDemo/Behaviors/Common/Strategies/EarlyTerminator.cs
+++ module/Demos/ComprehensiveDemo/Behaviors/Common/Strategies/EarlyTerminator.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class EarlyTerminator
 {
+    
+    /// <summary>
+    /// This kind of mimics our way of thinking. When we're doing a specific
+    /// subtask, we periodically recall previous tasks (callers), and 
+    /// see if those tasks' requirements are already met. If so, we can
+    /// simply terminate our current subtask. But, of course, complex
+    /// behaviors rise from having the option to terminate or not terminate
+    /// based on prior requirements, hence the choice of naming "Should
+    /// Terminate". We will return Status message rather than a boolean,
+    /// because termination is determined by both SUCCESS and FAILURE, which
+    /// are symmetric in some sense, but not entirely equivalent.
+    /// </summary>
+    /// <param name="memory">Stack Memory</param>
+    /// <returns>Status Message</returns>
     public static Status ShouldTerminate(Stack<Behavior> memory) {
         Status result = Status.RUNNING;
 

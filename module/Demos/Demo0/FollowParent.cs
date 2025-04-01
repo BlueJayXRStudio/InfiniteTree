8e48ab2b1c34b94a09e26b34e0c2b14864fe65ae
JayO <bluejayvrstudio@gmail.com>
2025-04-01 09:52:02 -0500

    testing

--- module/Demos/Demo0/FollowParent.cs
+++ module/Demos/Demo0/FollowParent.cs
@@ -1,34 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class FollowParent : Behavior
-{
-    private float velocity = 1.0f;
+// public class FollowParent : Behavior
+// {
+//     private float velocity = 1.0f;
 
-    public Status Step(Stack<Behavior> memory, GameObject go, Status message)
-    {
-        GameObject parent = go.GetComponent<ParentComponent>().GetParent;
+//     public Status Step(Stack<Behavior> memory, GameObject go, Status message)
+//     {
+//         GameObject parent = go.GetComponent<ParentComponent>().GetParent;
 
-        if (parent == null) {
-            memory.Push(this);
-            return Status.SUCCESS;
-        }
+//         if (parent == null) {
+//             memory.Push(this);
+//             return Status.SUCCESS;
+//         }
 
-        Vector3 ParentPos = parent.transform.position;
-        Vector3 CurrentPos = go.transform.position;
-        Vector3 diff = ParentPos - CurrentPos;
+//         Vector3 ParentPos = parent.transform.position;
+//         Vector3 CurrentPos = go.transform.position;
+//         Vector3 diff = ParentPos - CurrentPos;
 
-        if (diff.magnitude >= 0.02f) {
-            go.transform.position += diff.normalized * velocity * Time.deltaTime;
-            memory.Push(this);
-            return Status.RUNNING;
-        }
-        else {
-            // go.transform.position = parent.transform.position;
-            go.GetComponent<ParentComponent>().iterate();
-            memory.Push(this);
-            return Status.RUNNING;
-        }
-    }
-}
+//         if (diff.magnitude >= 0.02f) {
+//             go.transform.position += diff.normalized * velocity * Time.deltaTime;
+//             memory.Push(this);
+//             return Status.RUNNING;
+//         }
+//         else {
+//             // go.transform.position = parent.transform.position;
+//             go.GetComponent<ParentComponent>().iterate();
+//             memory.Push(this);
+//             return Status.RUNNING;
+//         }
+//     }
+// }
--- module/Demos/Demo2/RotateBehavior.cs
+++ module/Demos/Demo2/RotateBehavior.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class RotateBehavior : Behavior
-{
-    public Status Step(Stack<Behavior> memory, GameObject go, Status message)
-    {
-        go.transform.rotation = Quaternion.Euler(0, 180 * Time.deltaTime, 0) * go.transform.rotation;
-        memory.Push(this);
-        return Status.RUNNING;
-    }
-}
+// public class RotateBehavior : Behavior
+// {
+//     public Status Step(Stack<Behavior> memory, GameObject go, Status message)
+//     {
+//         go.transform.rotation = Quaternion.Euler(0, 180 * Time.deltaTime, 0) * go.transform.rotation;
+//         memory.Push(this);
+//         return Status.RUNNING;
+//     }
+// }
